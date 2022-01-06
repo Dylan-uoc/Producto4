784e447f14e6a683c27bec5ecb819d2828696806
Dylanuga <56144502+Dylanuga@users.noreply.github.com>
2022-01-06 20:14:35 +0100

    Con la visa de login creada

--- src/main/java/com/producto4/controller/ViewLoginController.java
+++ src/main/java/com/producto4/controller/ViewLoginController.java
@@ -7,15 +7,48 @@ package com.producto4.controller;
 
 import java.net.URL;
 import java.util.ResourceBundle;
+import javafx.event.ActionEvent;
+import javafx.fxml.FXML;
 import javafx.fxml.Initializable;
+import javafx.scene.control.*;
+import javafx.scene.input.*;
 
 /**
  * FXML Controller class
  *
  * @author rojaw
  */
-public class ViewLoginController implements Initializable {
+public class ViewLoginController implements Initializable  {
 
+    public ViewLoginController(){
+
+    }
+
+    @FXML
+    private TextField username_login;
+    
+    @FXML
+    private PasswordField password_login;
+    
+    @FXML
+    private Button button_login;
+    
+    @FXML
+    private Label wrong_login;
+    
+    @FXML
+    private void eventKey(KeyEvent event){
+    
+    
+    }
+    
+    @FXML
+    private void eventAction(ActionEvent event){
+
+
+    }
+    
+        
     /**
      * Initializes the controller class.
      */
